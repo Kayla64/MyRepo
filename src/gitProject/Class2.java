e253b263b8f3acd18110eed37b2937140aaccac9
zaria <zaria@10.154.0.137>
2022-11-26 19:10:20 -0500

    this is class2

---
+++ src/gitProject/Class2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gitProject;
+
+/**
+ * @author zaria
+ *
+ */
+public class Class2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
